f025ba36e53938173ced75f61b7f78f7fc0213dd
namdov3-blip <namdov3@gmail.com>
2025-10-13 02:11:55 +0700

    Update python.py

--- python.py
+++ python.py
@@ -191,7 +191,7 @@ hr {
   box-shadow: 0 1px 0 rgba(0,0,0,.02);
 }
 .info-card .label { 
-    font-size: 16px; 
+    font-size: 20px; 
     color: var(--primary-color); 
     font-weight: 700; 
     margin-bottom: 4px; 
